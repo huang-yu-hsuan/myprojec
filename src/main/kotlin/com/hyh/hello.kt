1d0e6123612a0e85913bb7e11a63ef2597df8dea
huang-yu-hsuan <qppleeeegggg@gmail.com>
2019-07-22 22:37:08 +0800

    Jave & Kotlin variable,data type

--- src/main/kotlin/com/hyh/hello.kt
+++ src/main/kotlin/com/hyh/hello.kt
@@ -3,8 +3,14 @@ package com.hyh
 fun main(args: Array<String>) {
 //    println("Hello Kotlin")
 //    Human().hello();
-   val h = Human()
+    val h = Human()
     h.hello()
+    var age = 19
+    age = 20
+    var weight = 66.5
+    val name : String
+    name = "huang"
+
 }
 
 class Human {
